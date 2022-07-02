8dde4d7609643444d4d37967a7bb48076ee04d2e
Kristina Nikolic <kristinanikolic.n01@gmail.com>
2022-07-02 20:11:03 +0200

    Update

--- dbconnect.php
+++ dbconnect.php
@@ -4,7 +4,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
-$con = mysqli_connect("localhost", "root", "N999k1219", 'Projekat');
+$con = mysqli_connect("localhost", "root", "", 'Projekat');
 
 if(mysqli_connect_errno()){
     exit ("Greska u konekciji");
