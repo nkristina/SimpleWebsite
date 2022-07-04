a1aa013ba4b72aa927e92257bfd1032ae9f35db4
Kristina Nikolic <kristinanikolic.n01@gmail.com>
2022-07-04 16:27:38 +0200

    Jos opcija

--- prviLogin.php
+++ prviLogin.php
@@ -21,20 +21,21 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         ?>
         <div class='header'><h1>Dodatni podaci za registraciju</h1></div>
         <h3>Molimo popunite sledece podatke:</h3>
+        
+        <?php
+        if(!isset($_SESSION['brojKase']))  $_SESSION['brojKase']= 1;
+        if(!isset($_SESSION['brojRacune']))  $_SESSION['brojRacune']= 1;
+        if(!isset($_SESSION['brojSifre']))  $_SESSION['brojSifre']= 1;?> 
+        
         <form action="prviLoginKod.php" method="post" name="formaPrviLog" onsubmit="return proveriPrvi();">
             <table>
+                <?php 
+                for($k=0; $k<$_SESSION['brojSifre']; $k++){?>
                 <tr>
-                    <td>Sifra delatnosti 1:</td>
+                    <td>Sifra delatnosti:</td>
                     <td><input type="text" name="sifra[]"></td>
                 </tr>
-                <tr>
-                    <td>Sifra delatnosti 2:</td>
-                    <td><input type="text" name="sifra[]"></td>
-                </tr>
-                <tr>
-                    <td>Sifra delatnosti 3:</td>
-                    <td><input type="text" name="sifra[]"></td>
-                </tr>
+                <?php } ?>
                 <tr>
                     <td>Da li je preduzece u sistemu PDV:</td>
                     <td><input type="checkbox" name="pdv" value="1">DA</td>
@@ -43,76 +44,57 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                     <td>Poslovni ziro racini:</td>
                     <td>&nbsp;</td>
                 </tr>
+                <?php 
+                for($i=0; $i<$_SESSION['brojRacune']; $i++){?>
                 <tr>
-                    <td>Banka 1:</td>
+                    <td>Banka:</td>
                     <td><input type="text" name="banka[]"></td>
                 </tr>
                 <tr>
-                    <td>Racun 1:</td>
+                    <td>Racun:</td>
                     <td><input type="text" name="racun[]"></td>
                 </tr>
-                <tr>
-                    <td>Banka 2:</td>
-                    <td><input type="text" name="banka[]"></td>
-                </tr>
-                <tr>
-                    <td>Racun 2:</td>
-                    <td><input type="text" name="racun[]"></td>
-                </tr>
-                <tr>
-                    <td>Banka 3:</td>
-                    <td><input type="text" name="banka[]"></td>
-                </tr>
-                <tr>
-                    <td>Racun 3:</td>
-                    <td><input type="text" name="racun[]"></td>
-                </tr>
+                <?php } ?> 
                 <tr>
                     <td>Broj kasa:</td>
-                    <td><input type="number" name="broj" value="1"></td>
+                    <td><input type="number" name="broj" value="<?php if(isset($_SESSION['brojKase'])) echo $_SESSION['brojKase']; else echo 1;?>"></td>
                 </tr>
+                <?php for($j=0; $j<$_SESSION['brojKase']; $j++){?>
                 <tr>
-                    <td>Tip 1:</td>
+                    <td>Tip:</td>
                     <td><select name="tip[]">
                         <option>Galeb Pro</option>
                         <option>Galeb N910</option>
                         <option>Galeb XP</option>
                         </select></td>
-                </tr>
+                </tr> 
                 <tr>
-                    <td>Objekat 1:</td>
+                    <td>Objekat:</td>
                     <td><input type="text" name="objekat[]"></td>
                 </tr>
-                <tr>
-                    <td>Tip 2:</td>
-                    <td><select name="tip[]">
-                        <option>Galeb Pro</option>
-                        <option>Galeb N910</option>
-                        <option>Galeb XP</option>
-                        </select></td>
-                </tr>
-                <tr>
-                    <td>Objekat 2:</td>
-                    <td><input type="text" name="objekat[]"></td>
-                </tr>
-                <tr>
-                    <td>Tip 3:</td>
-                    <td><select name="tip[]">
-                        <option>Galeb Pro</option>
-                        <option>Galeb N910</option>
-                        <option>Galeb XP</option>
-                        </select></td>
-                </tr>
-                <tr>
-                    <td>Objekat 3:</td>
-                    <td><input type="text" name="objekat[]"></td>
-                </tr>
+                <?php } ?>
                 <tr>
                     <td><input type="submit" name="unesi" value="Unesi i nastavi dalje" class='dugme'></td>
                     <td>&nbsp;</td>
                 </tr>
-            </table>
+                </table>
         </form>
+        <form method="post">
+            <input type="submit" name="kasaPlus" value="Dodaj polje za kasu" class="dugme">
+            <input type="submit" name="racunPlus" value="Dodaj polje za racun" class="dugme">  
+            <input type="submit" name="sifraPlus" value="Dodaj polje za sifru delatnosti" class="dugme">   
+        </form>
+        <?php 
+        if(isset($_POST['kasaPlus'])){
+            $_SESSION['brojKase']++;
+        }
+        if(isset($_POST['racunPlus'])){
+            $_SESSION['brojRacune']++;
+        }
+        if(isset($_POST['sifraPlus'])){
+            $_SESSION['brojSifre']++;
+        }
+        ?>
         <p>Mocicete da menjate ove podatke i kasnije.</p>
         <div class='footer'>
         <h4>Hvala sto koristite nas sajt!</h4>
